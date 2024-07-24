2a081464b5858cb114f1f1e45d23d2a44c55d114
Akmal Sindarov <asauz777@gmail.com>
2024-07-24 19:02:56 +1000

    Create database.go

---
+++ database.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := "host=localhost user=postgres password=superUser7 dbname=marina port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database!", err)
+	}
+
+	DB.AutoMigrate(&Todo{})
+}
